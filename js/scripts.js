5983abff8a38c6521cd63b3c13d51428ffbcf40c
sanjar1311 <sanjarabduxamidov1311@gmail.com>
2021-01-27 22:59:27 -0600

    add js and complate project

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,21 @@ var createElement = function (tagName, className, text) {
 
   return element;
 };
+
+
+var colors = ['white','yellow','orange','gold','red','blue','grey','black','green','coral','tomato','pink'];
+
+
+var elBtn = $_('.btn');
+
+function getRandomNumber(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+function changeBgcolor() {
+  for(var i = 0; i < colors.length; i++) {
+    document.body.style.backgroundColor = `${colors[getRandomNumber(colors)]}`;
+  }
+}
+
+elBtn.addEventListener('click', changeBgcolor);
